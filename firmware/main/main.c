705f2ddac34d8fdece1f89f5ab0d3bb1a6f01e70
Oliver <oliver.roman49@hotmail.com>
2024-11-27 01:16:43 +1000

    First test build

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -3,13 +3,13 @@
  * @author Oliver Roman
  * @brief Application main entry point
  * @version 0.1
- * @date 2024-11-04
+ * @date 2024-11-25
  */
 
 #include <stdio.h>
+#include "driver/gpio.h"
 
-
-void main(void) {
+int app_main() {
     
     int err = -1;
     printf("Initialising board... \n");
@@ -17,11 +17,13 @@ void main(void) {
     // Add err = board_init function
     if (err) {
         printf("Board failed to initialise with error: %d\n", err);
-        return;
+        return err;
     }
 
     // Add err = bluetooth_init function
     if (err) {
         printf("Bluetooth failed to initialise with error: %d\n", err);
     }
+
+    return err;
 }
